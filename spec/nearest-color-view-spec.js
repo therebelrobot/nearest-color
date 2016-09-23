3d88356e1225709fc56b650b3e0e180707413828
Trent Oswald <trentoswald@therebelrobot.com>
2016-09-23 08:55:38 -0700

    Initial plugin files

---
+++ spec/nearest-color-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NearestColorView from '../lib/nearest-color-view';
+
+describe('NearestColorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
